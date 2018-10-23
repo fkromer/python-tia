f051b6f5e78e0fe8d7b0ed1fdd2f832b1385a316
fkromer <florian.kromer@mailbox.org>
2018-10-23 14:15:07 +0200

    improve config handling
    * improve config file reading
    
    * add basic config validation (overall config, pipelines config)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,16 @@
-from tia.config import read_file
+from tia.config import (
+    read_file,
+    read_and_validate_config,
+    is_pipelines_config_valid,
+    ConfigError,
+    CONFIG_FILE_NAME
+)
 from pytest import raises
+from strictyaml import YAML
+from collections import OrderedDict
 
-def test_reading_existing_config_file_returns_string(tmpdir):
-    cf = tmpdir.mkdir("subdir").join("tia.yaml")
+def test_reading_existing_valid_config_file_returns_string(tmpdir):
+    cf = tmpdir.mkdir("subdir").join(CONFIG_FILE_NAME)
     expected_str = """
     some
     multi
@@ -13,8 +21,73 @@ def test_reading_existing_config_file_returns_string(tmpdir):
     file_path = str(cf)
     assert read_file(file_path) == expected_str
 
-def test_reading_non_existing_config_file_raises_exception():
-    file_path = "/nonexisting"
+def test_reading_existing_invalid_config_file_raises_error(tmpdir):
+    invalid_config_file = "tia.ini"
+    cf = tmpdir.mkdir("subdir").join(invalid_config_file)
+    file_content = "irrelevant"
+    cf.write(file_content)  # required to create file
+    file_path = str(cf)
     expected_str = "irrelevant"
-    with raises(Exception):
+    with raises(ConfigError):
         assert read_file(file_path) == expected_str
+
+def test_reading_non_existing_config_file_raises_exception():
+    file_path = "/nonexisting/" + CONFIG_FILE_NAME
+    expected_str = "irrelevant"
+    with raises(FileNotFoundError):
+        assert read_file(file_path) == expected_str
+
+def test_read_valid_parent_key_config():
+    yaml_config = """
+    pipelines:
+    """
+    assert read_and_validate_config(yaml_config) == YAML(OrderedDict([('pipelines', '')]))
+
+def test_read_valid_pipelines_config():
+    yaml_pipelines_config = """
+    pipelines:
+    - name: pytest
+      dirs:
+      - path:       /foo_dir
+        full-scope: yes
+      - path:       /bar_dir
+        full-scope: no
+      files:
+      - path:       foo_file.py
+        full-scope: yes
+      - path:       bar_file.py
+        full-scope: no
+    - name: pylint
+      dirs:
+      - path:       /baz_dir
+        full-scope: no
+      files:
+      - path:       baz_file.ini
+        full-scope: yes
+    """
+    yaml_pipelines = read_and_validate_config(yaml_pipelines_config)
+    expected_yaml_instance = YAML(OrderedDict([('pipelines', [OrderedDict([('name', 'pytest'), ('dirs', [OrderedDict([('path', '/foo_dir'), ('full-scope', True)]), OrderedDict([('path', '/bar_dir'), ('full-scope', False)])]), ('files', [OrderedDict([('path', 'foo_file.py'), ('full-scope', True)]), OrderedDict([('path', 'bar_file.py'), ('full-scope', False)])])]), OrderedDict([('name', 'pylint'), ('dirs', [OrderedDict([('path', '/baz_dir'), ('full-scope', False)])]), ('files', [OrderedDict([('path', 'baz_file.ini'), ('full-scope', True)])])])])]))
+    assert is_pipelines_config_valid(yaml_pipelines) == True
+    assert yaml_pipelines == expected_yaml_instance
+
+def test_read_invalid_pipelines_config():
+    yaml_pipelines_config = """
+    pipelines:
+    - name: pytest
+      dirs:
+      - full-scope: yes
+      - path:       /bar_dir
+        full-scope: no
+      - path:       bar_file.py
+        full-scope: no
+    - name: pylint
+      dirs:
+      - path:       /baz_dir
+        full-scope: no
+      files:
+      - path:       baz_file.ini
+        full-scope: yes
+    """
+    config = read_and_validate_config(yaml_pipelines_config)
+    yaml_pipelines = config['pipelines']
+    assert is_pipelines_config_valid(yaml_pipelines) == False
--- tia/config.py
+++ tia/config.py
@@ -1,14 +1,36 @@
 from typing import AnyStr
 from pathlib import Path
+from strictyaml import YAML, Map, Seq, Any, Bool, load, Str
 
-def read_file(file_path: AnyStr) -> str:
+CONFIG_FILE_NAME: str = 'tia.yaml'
+
+class ConfigError(Exception):
+    pass
+
+def read_file(file_path: str) -> str:
     """
     file_path type annotation:
     os.PathLike type annotation not ready yet https://github.com/python/mypy/issues/5667
     """
     config_file = Path(file_path)
     if not config_file.is_file():
-        raise FileNotFoundError
-    with open(file_path, 'r') as cf:
-        config = cf.read()
-    return config
+        raise FileNotFoundError('Config file {name} not found.'.format(name=config_file.name))
+    if not config_file.name == CONFIG_FILE_NAME:
+        raise ConfigError('Invalid config file name {name}.'.format(name=config_file.name))
+    return config_file.read_text()
+
+def read_and_validate_config(strictyaml_config: str) -> YAML:
+    config_schema = Map({"pipelines": Any()})
+    return load(strictyaml_config, config_schema)
+
+def is_pipelines_config_valid(strictyaml_pipelines: YAML) -> YAML:
+    pipelines_schema = Map({"pipelines": Seq(Map({
+        "name": Str(),
+        "dirs": Seq(Map({"path": Str(), "full-scope": Bool()})),
+        "files": Seq(Map({"path": Str(), "full-scope": Bool()}))
+        }))})
+    try:
+        strictyaml_pipelines.revalidate(pipelines_schema)
+        return True
+    except:
+        return False
